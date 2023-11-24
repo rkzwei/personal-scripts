1096c57fa2d234f00904a62c4c857b0a7603d8a7
rk <rk@rkzwei-deb.rkzwei.dev>
2023-11-24 05:07:23 -0300

    updated script to run from anywhere

--- tracker-updater.sh
+++ tracker-updater.sh
@@ -3,11 +3,16 @@
 # Set the URL of the trackers list file
 TRACKERS_URL="https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
 
-# Set the path to the qBittorrent.conf file
-QBITTORRENT_CONF="/home/qbtuser/.config/qBittorrent/qBittorrent.conf"
+# Find qBittorrent.conf in the entire filesystem
+QBITTORRENT_CONF=$(sudo find / -name 'qBittorrent.conf' -type f 2>/dev/null)
+
+if [ -z "$QBITTORRENT_CONF" ]; then
+  echo "Error: qBittorrent.conf not found."
+  exit 1
+fi
 
 # Backup the original qBittorrent.conf file
-cp "$QBITTORRENT_CONF" "$QBITTORRENT_CONF.backup"
+sudo cp "$QBITTORRENT_CONF" "$QBITTORRENT_CONF.backup"
 
 # Download the new trackers list
 curl -o /tmp/trackers_best.txt "$TRACKERS_URL"
@@ -16,7 +21,7 @@ curl -o /tmp/trackers_best.txt "$TRACKERS_URL"
 NEW_TRACKERS=$(sed ':a;N;$!ba;s/\n/\\n/g' /tmp/trackers_best.txt)
 
 # Replace the content of AdditionalTrackers in qBittorrent.conf
-sed -i "s|\(Session\\AdditionalTrackers=\).*|\1${NEW_TRACKERS}|" "$QBITTORRENT_CONF"
+sudo sed -i "s|\(Session\\AdditionalTrackers=\).*|\1${NEW_TRACKERS}|" "$QBITTORRENT_CONF"
 
 # Clean up the temporary file
 rm /tmp/trackers_best.txt
